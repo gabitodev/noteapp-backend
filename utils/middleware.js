5123b775f1c54721429efd59182fa4dfdaabbf4f
gabitodev <gabitodev@gmail.com>
2022-05-28 18:17:31 -0400

    finished tests for the user router

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,8 +2,11 @@ const logger = require('./logger');
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
+  logger.error(error.name);
   if (error.name === 'ValidationError') {
-    response.status(400).json({error: 'username length must be more than 6 characters'})
+    return response.status(400).json({error: 'username length must be more than 6 characters'});
+  } else if (error.name === 'CastError') {
+    return response.status(401).json({error: 'invalid user'})
   }
   next(error);
 };
