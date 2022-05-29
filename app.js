69fd183dcbe2f0e52cfe90ec44c66d16e2b76410
gabitodev <gabitodev@gmail.com>
2022-05-29 09:37:44 -0400

    controller login created, tests passed, added router to app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const mongoose = require('mongoose');
 const middleware = require('./utils/middleware');
 
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.errorHandler);
 
---
+++ test/login_api.test.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+describe('login router', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash('secret.65', saltRounds);
+    const user = new User({username: 'test123', name:'el tester', passwordHash});
+    await user.save();
+  }, 100000);
+
+  test('suceeds with code 200 if credentials are correct', async () => {
+
+    const user = {
+      username: 'test123',
+      password: 'secret.65'
+    };
+
+    const response = await api
+      .post('/api/login')
+      .send(user)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.username).toBe(user.username);
+    expect(response.body.name).toBe('el tester');
+
+  });
+
+  test('fails with code 401 if username is incorrect', async () => {
+
+    const user = {
+      username: 'test1234456',
+      password: 'secret.65'
+    };
+
+    const response = await api
+      .post('/api/login')
+      .send(user)
+      .expect(401)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.error).toBe('invalid username or password');
+  });
+
+  test('fails with code 401 if password is incorrect', async () => {
+
+    const user = {
+      username: 'test123',
+      password: 'secretooooo'
+    };
+
+    const response = await api
+      .post('/api/login')
+      .send(user)
+      .expect(401)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.error).toBe('invalid username or password');
+  });
+});
